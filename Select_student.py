39d863b654fdaa9c0fe5f282d33c69598bf780d6
ShigaKotsu <139493140+ShigaKotsu@users.noreply.github.com>
2023-09-28 17:01:31 -0300

    student list functions

---
+++ Select_student.py
@@ -0,0 +1,34 @@
+from random import choice
+list_student=open("alunos.txt","r")
+n=list_student.read()
+b=list(map(str,n.split("\n")))
+
+def phrase_randon(list):
+    print(choice(list))
+
+def add_aluno(lista):
+    insert=str(input("digite nome um aluno: "))
+    file=open("alunos.txt","a")
+    file.write("\n"+ insert)
+    lista.append(insert)
+    
+  
+def remove(lista):
+    delete_name=str(input("digite um nome: "))
+    with open("alunos.txt","w") as file:
+        file.write("")
+    lista.remove(delete_name)
+    with open("alunos.txt","a") as file:
+        for n in lista:
+            file.write(n+"\n")
+
+
+def main():
+    list_student=open("alunos.txt","r")
+    n=list_student.read()
+    b=list(map(str,n.split("\n")))
+
+while True:
+    print("1.Random Choice\n2.Add Student\n3.Remove name in list")
+    c= int(input("what do you want?: "))
+        
